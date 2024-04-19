ed6e6405caba1dd8969d0f28349a4549e9a7e998
Oulaya omar <oulaya.omar@gmail.com>
2024-04-19 18:22:42 +0200

    btn manage

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,69 +1,95 @@
-/* *************************************************** */ /* PARTIE 1 ==> DECLARATION DES VARIABLES DU PROGRAMME */ /* *************************************************** */ let scoresGlobals = [0, 0];
+
+//***************** Déclaration des variables****************************************************************
 let scoreEnCours = 0;
-score1= document.getElementById(`score-1`) ;
-score2=document.getElementById(`score-2`);
-encours1= document.getElementById(`encours-1`);
-encours2= document.getElementById(`encours-2`);
-const joueur1 = document.querySelector('.joueur1');
-const joueur2 = document.querySelector('.joueur2');
-let lancer = document.querySelector(`.btn-lancer`);
-let nouveauJeu = document.querySelector('.btn-nouveau');
+const scoreElements = [document.getElementById('score-1'), document.getElementById('score-2')];
+const encoursElements = [document.getElementById('encours-1'), document.getElementById('encours-2')];
+const joueurs = [document.querySelector('.joueur1'), document.querySelector('.joueur2')];
+const lancer = document.querySelector('.btn-lancer');
+const nouveauJeu = document.querySelector('.btn-nouveau');
+const de = document.querySelector('.de');
+const btnSelection = document.querySelector('.btn-selection');
+const btnCommuter = document.querySelector('.btn-commuter');
+const nomJoueur = [document.getElementById('nom-1'), document.getElementById('nom-2')]
 
 
-/* ***************************************** */ /* PARTIE 2 ==> INITIALISATION DU GUI DU JEU */ /* ***************************************** */
-nouveauJeu.addEventListener("click", () => {
-    document.querySelector(`.de`).style.display = 'none';
-    document.querySelector(`.btn-selection`).style.display = 'none'
-    score1.textContent=0;
-    score2.textContent=0;
-    encours1.textContent=0;
-    encours2.textContent=0;
+
+
+//***************** Initialisation du GUI du jeu****************************************************************
+nouveauJeu.addEventListener('click', () => {
+    de.style.display = 'none';
+    btnSelection.style.display = 'none';
+    joueurs.forEach(joueur=> joueur.classList.remove('active'));
+    nomJoueur.forEach((nom,index)=>{
+        nom.textContent = 'joueur  ' +`${index+1}`;
+        nom.classList.remove('vainqueur');})
+    scoreElements.forEach(score => score.textContent = 0);
+    encoursElements.forEach(encours => encours.textContent = 0);
+    lancer.style.display = 'block';
     scoreEnCours = 0;
 });
 
-/* ********************************************************** */ /* PARTIE 3 ==> GESTION DU CLICK SUR LE BOUTON 'LANCEZ LE DE' */ /* ********************************************************** */
-
-
-
-lancer.addEventListener("click", () => {    
-    
-        if(joueur1.classList.contains('active')){
+// *****************Gestion du clic sur le bouton "Lancer le dé"****************************************************************
+lancer.addEventListener('click', () => {
+    const joueurActif = joueurs.find(joueur => joueur.classList.contains('active'));
+    const index = joueurs.indexOf(joueurActif);
+    if (joueurActif) {
+        const deValeur = Math.floor(Math.random() * 6) + 1;
+        de.style.display = 'block';
+        de.src = `resources/images/de-${deValeur}.png`;
+        if (deValeur !== 1)
+            {
+            scoreEnCours += deValeur;
             
-            let de = Math.floor(Math.random() * 6) + 1;
-            let domDe = document.querySelector(".de");
-            domDe.style.display = "block";
-            domDe.src = `resources/images/de-${de}.png`;
-            scoreEnCours += de;
-            encours1.textContent = scoreEnCours;
-           
+            encoursElements[index].textContent = scoreEnCours;
+            }else{
+                joueurs.forEach((joueur) => {
+                    joueur.classList.toggle('active');
+                });
+                encoursElements[index].textContent = 0;
+                scoreEnCours = 0;
+            }
+        if(scoreElements[index].textContent >= 30){
+            nomJoueur[index].classList.add('vainqueur');
+            nomJoueur[index].textContent = 'bravo !!!!';
+            lancer.style.display = 'none';
+        }      
         }
-        if(joueur2.classList.contains('active')){
-            scoreEnCours = 0;
-            let de = Math.floor(Math.random() * 6) + 1;
-            let domDe = document.querySelector(".de");
-            domDe.style.display = "block";
-            domDe.src = `resources/images/de-${de}.png`;
-            scoreEnCours += de;
-            encours2.textContent  = scoreEnCours;
+    if (!joueurs.some(joueur => joueur.classList.contains('active'))) {
+            btnSelection.style.display = 'block';
             
         }
-        else{document.querySelector(`.btn-selection`).style.display = 'block';}   
-    
+});
+
+//***************** Sélection du joueur actif****************************************************************
+joueurs.forEach((joueur, index) => {
+    joueur.addEventListener('click', () => {
+        joueur.classList.toggle('active');
+        if (joueurs[(index + 1) % 2].classList.contains('active')) {
+            joueurs[(index + 1) % 2].classList.remove('active');
+        }
+        btnSelection.style.display = 'none';
     });
+});
 
-//************* Joueur actif ********************* */
 
-joueur1.addEventListener('click', () => {
-    joueur1.classList.toggle('active')
-    if (joueur2.classList.contains('active')) {
-        joueur2.classList.remove('active');        
+//***************** changer de joueur ****************************************************************
+btnCommuter.addEventListener('click', () => {
+    const joueurActif = joueurs.find(joueur => joueur.classList.contains('active'));
+    const index = joueurs.indexOf(joueurActif);
+    joueurs.forEach((joueur) => {
+        joueur.classList.toggle('active');
+    });
+    if(joueurActif){
+        let  scroreActuel = parseInt(scoreElements[index].textContent) + scoreEnCours; 
+        scoreElements[index].textContent = scroreActuel;
+        encoursElements[index].textContent = 0;
+        scoreEnCours =0; 
+        if(scoreElements[index].textContent>=30){
+            nomJoueur[index].classList.add('vainqueur');
+            nomJoueur[index].textContent = 'bravo!!!!';
+            lancer.style.display = 'none';
+        }      
+
     }
-    document.querySelector(`.btn-selection`).style.display = 'none';
-})
-joueur2.addEventListener('click', () => {
-    joueur2.classList.toggle('active')
-    if (joueur1.classList.contains('active')) {
-        joueur1.classList.remove('active');       
-    } 
-    document.querySelector(`.btn-selection`).style.display = 'none';
-})
+});
+// *************les régles du jeu************************************************************************************************
