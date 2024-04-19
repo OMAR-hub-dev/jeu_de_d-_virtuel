7b47171de83671d7c5a5a61449d0b769c550429d
Oulaya omar <oulaya.omar@gmail.com>
2024-04-19 11:48:53 +0200

    1 steep

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,52 +1,69 @@
-/* *************************************************** */ /* PARTIE 1 ==> DECLARATION DES VARIABLES DU PROGRAMME */ /* *************************************************** */ let scoresGlobals =[0, 0];
+/* *************************************************** */ /* PARTIE 1 ==> DECLARATION DES VARIABLES DU PROGRAMME */ /* *************************************************** */ let scoresGlobals = [0, 0];
 let scoreEnCours = 0;
-let joueurActif = 1;
-
-/* ***************************************** */ /* PARTIE 2 ==> INITIALISATION DU GUI DU JEU */ /* ***************************************** */ 
-document.getElementById(`score-1`).textContent = '0'; 
-document.getElementById(`score-2`).textContent = '0'; 
-document.getElementById(`encours-1`).textContent = '0'; 
-document.getElementById(`encours-2`).textContent = '0'; 
-document.querySelector(`.de`).style.display = 'none';
-
-/* ********************************************************** */ /* PARTIE 3 ==> GESTION DU CLICK SUR LE BOUTON 'LANCEZ LE DE' */ /* ********************************************************** */
-
+score1= document.getElementById(`score-1`) ;
+score2=document.getElementById(`score-2`);
+encours1= document.getElementById(`encours-1`);
+encours2= document.getElementById(`encours-2`);
+const joueur1 = document.querySelector('.joueur1');
+const joueur2 = document.querySelector('.joueur2');
 let lancer = document.querySelector(`.btn-lancer`);
-
-lancer.addEventListener("click", () => {
-let de = Math.floor(Math.random() * 6) + 1;
-
-let domDe = document.querySelector(".de");
-domDe.style.display = "block";
-domDe.src = `resources/images/de-${de}.png`;
-scoreEnCours += de;
-document.getElementById(`encours-${joueurActif}`).textContent = scoreEnCours;
-
-});
-// ***********nouveau jeu ********************************
 let nouveauJeu = document.querySelector('.btn-nouveau');
-nouveauJeu.addEventListener("click", () =>{
-    document.getElementById(`score-1`).textContent = '0'; 
-    document.getElementById(`score-2`).textContent = '0'; 
-    document.getElementById(`encours-1`).textContent = '0'; 
-    document.getElementById(`encours-2`).textContent = '0'; 
+
+
+/* ***************************************** */ /* PARTIE 2 ==> INITIALISATION DU GUI DU JEU */ /* ***************************************** */
+nouveauJeu.addEventListener("click", () => {
     document.querySelector(`.de`).style.display = 'none';
+    document.querySelector(`.btn-selection`).style.display = 'none'
+    score1.textContent=0;
+    score2.textContent=0;
+    encours1.textContent=0;
+    encours2.textContent=0;
     scoreEnCours = 0;
 });
 
+/* ********************************************************** */ /* PARTIE 3 ==> GESTION DU CLICK SUR LE BOUTON 'LANCEZ LE DE' */ /* ********************************************************** */
+
+
+
+lancer.addEventListener("click", () => {    
+    
+        if(joueur1.classList.contains('active')){
+            
+            let de = Math.floor(Math.random() * 6) + 1;
+            let domDe = document.querySelector(".de");
+            domDe.style.display = "block";
+            domDe.src = `resources/images/de-${de}.png`;
+            scoreEnCours += de;
+            encours1.textContent = scoreEnCours;
+           
+        }
+        if(joueur2.classList.contains('active')){
+            scoreEnCours = 0;
+            let de = Math.floor(Math.random() * 6) + 1;
+            let domDe = document.querySelector(".de");
+            domDe.style.display = "block";
+            domDe.src = `resources/images/de-${de}.png`;
+            scoreEnCours += de;
+            encours2.textContent  = scoreEnCours;
+            
+        }
+        else{document.querySelector(`.btn-selection`).style.display = 'block';}   
+    
+    });
 
 //************* Joueur actif ********************* */
-const joueur1 = document.querySelector('.joueur_panel_1');
-const joueur2 = document.querySelector('.joueur_panel_2');
-joueur1.addEventListener('click', ()=>{
+
+joueur1.addEventListener('click', () => {
     joueur1.classList.toggle('active')
-    if(joueur2.classList.contains('active')){
-        joueur2.classList.remove('active');
-    }    
+    if (joueur2.classList.contains('active')) {
+        joueur2.classList.remove('active');        
+    }
+    document.querySelector(`.btn-selection`).style.display = 'none';
 })
-joueur2.addEventListener('click', ()=>{
+joueur2.addEventListener('click', () => {
     joueur2.classList.toggle('active')
-    if(joueur1.classList.contains('active')){
-        joueur1.classList.remove('active') ;
-    }R    
+    if (joueur1.classList.contains('active')) {
+        joueur1.classList.remove('active');       
+    } 
+    document.querySelector(`.btn-selection`).style.display = 'none';
 })
